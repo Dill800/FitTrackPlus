9c962e3407d9c89ddc2229702b913c6e56753442
McGovern <dillonmcgovern@ufl.edu>
2022-01-26 17:53:37 -0500

    background

--- screens/Login.js
+++ screens/Login.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Text, ScrollView, ImageBackground, Dimensions, View} from 'react-native';
+
 
 import {
     StyledContainer,
@@ -7,6 +9,7 @@ import {
     PageTitle
 } from './../components/styles'
 
+/*
 const Login = () => {
     return (
         <StyledContainer>
@@ -16,6 +19,31 @@ const Login = () => {
             </InnerContainer>
         </StyledContainer>
     );
+}
+*/
+
+const Login = ({navigation}) => {
+
+    return (
+        <ScrollView 
+        style={{flex: 1, backgroundColor: '#ffffff'}}
+        showsVerticalScrollIndicator={false}>
+            <ImageBackground
+            source={require('./../assets/background.jpg')}
+            style={{
+                height: Dimensions.get('window').height / 2.5,
+            }}>
+            
+            <StyledContainer>
+            <InnerContainer>
+                <PageLogo resizeMode="cover" source={require('./../assets/logo3.png')}></PageLogo>
+                    
+            </InnerContainer>
+        </StyledContainer>
+            </ImageBackground>
+        </ScrollView>
+    );
+
 }
 
 
