0c05c54681c11680ff6e950d8a2687c0e2b9015b
Ryan Ahmed <ra700@hotmail.com>
2022-04-07 02:18:22 -0400

    boomer code to epic bro code

--- screens/Home.js
+++ screens/Home.js
@@ -5,96 +5,10 @@ import Donut from '../navigation/Donut'
 
 import {ScrollView, StyleSheet, Text, View, TouchableOpacity, StatusBar} from "react-native";
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      goalDailyActivity: "",
-      activities: [],
-    };
-  }
-  static navigationOptions = {
-    title: "Home",
-  };
-
-  async componentDidMount() {
-    const username = this.props.navigation.state.params.username;
-    const token = this.props.navigation.state.params.token;
-    let response = await fetch(
-      "https://avigael-shop-fitness.herokuapp.com/users/" + username,
-      {
-        method: "GET",
-        headers: {
-          Accept: "*/*",
-          "x-access-token": token,
-          Connection: "keep-alive",
-          "cache-control": "no-cache",
-        },
-      }
-    );
-
-    response = await response.json();
-    let activities = await fetch(
-      "https://avigael-shop-fitness.herokuapp.com/activities",
-      {
-        method: "GET",
-        headers: {
-          Accept: "*/*",
-          "x-access-token": token,
-          Connection: "keep-alive",
-          "cache-control": "no-cache",
-        },
-      }
-    );
-    activities = await activities.json();
-    this.setState({
-      goalDailyActivity: response.goalDailyActivity,
-      activities: activities.activities,
-    });
-  }
-
-  async refresh() {
-    this.setState({ goalDailyActivity: "", activities: [] });
-    const username = this.props.navigation.state.params.username;
-    const token = this.props.navigation.state.params.token;
-    let response = await fetch(
-      "https://avigael-shop-fitness.herokuapp.com/users/" + username,
-      {
-        method: "GET",
-        headers: {
-          Accept: "*/*",
-          "x-access-token": token,
-          Connection: "keep-alive",
-          "cache-control": "no-cache",
-        },
-      }
-    );
-
-    response = await response.json();
-    let activities = await fetch(
-      "https://avigael-shop-fitness.herokuapp.com/activities",
-      {
-        method: "GET",
-        headers: {
-          Accept: "*/*",
-          "x-access-token": token,
-          Connection: "keep-alive",
-          "cache-control": "no-cache",
-        },
-      }
-    );
-    activities = await activities.json();
-    this.setState({
-      goalDailyActivity: response.goalDailyActivity,
-      activities: activities.activities,
-    });
-  }
-  
-
-  render() {
+const Home = ({navigation}) => {
     let exercise = [];
     let activityTotal = 0.0;
-    const activities = this.state.activities;
+    const activities = [];
     const widthAndHeight = 250
     const series = [123, 321, 90]
     const sliceColor = ['#F44336','#2196F3','#FFEB3B']
@@ -270,173 +184,176 @@ class Home extends React.Component {
         dataLabel: "carb"
       }];
 
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        flexDirection: "column",
-        justifyContent: "flex-start",
-        alignItems: "center",
-      },
-      progress: {
-        width: "95%",
-        height: 125,
-        marginTop: 15,
-        alignItems: "center",
-      },
-      title_box: {
-        backgroundColor: "rgba(74,144,226,1)",
-        borderRadius: 10,
-        width: "95%",
-        height: 40,
-        justifyContent: "center",
-      },
-      title: {
-        color: "rgba(255,255,255,1)",
-        fontSize: 22,
-        alignSelf: "center",
-      },
-      progress_container: {
-        flexDirection: "row",
-        width: "100%",
-        marginTop: 20,
-        justifyContent: "center",
-      },
-      progress_box: {
-        backgroundColor: "rgba(213,218,223,1)",
-        width: "40%",
-        height: 55,
-        borderRadius: 10,
-        marginHorizontal: 25,
-      },
-      progress_title: {
-        color: "#121212",
-        alignSelf: "center",
-        marginVertical: 4,
-      },
-      progress_value: {
-        color: "#121212",
-        fontSize: 20,
-        fontWeight: "bold",
-        alignSelf: "center",
-      },
-      exercise_container: {
-        width: "95%",
-        height: 400,
-        alignItems: "center",
-      },
-      box: {
-        backgroundColor: "rgba(213,218,223,1)",
-        borderRadius: 10,
-        width: "95%",
-        height: 275,
-        alignSelf: "center",
-      },
-      btn_box: {
-        flexDirection: "row",
-        width: "75%",
-        justifyContent: "center",
-      },
-      btn_shape: {
-        backgroundColor: "rgba(178,108,233,1)",
-        borderRadius: 10,
-        width: "50%",
-        height: 40,
-        marginTop: 10,
-        justifyContent: "center",
-      },
-      btn_text: {
-        color: "rgba(255,255,255,1)",
-        fontSize: 16,
-        textAlign: "center",
-        fontWeight: "bold",
-      },
-    });
-
-    return (
-      <View style={styles.container}>
-        <View style={styles.progress}>
-          <View style={styles.title_box}>
-            <Text style={styles.title}>"Hi, Frank!" 👋</Text>
-          </View>
-          <View style={styles.progress_container}>
-            <View style={styles.progress_box}>
-              <Text style={styles.progress_title}>Current Weight:</Text>
-              <Text style={styles.progress_value}>
-                {"155 lbs 🔼"}
-              </Text>
+      return (
+        <View style={styles.container}>
+          <View style={styles.progress}>
+            <View style={styles.title_box}>
+              <Text style={styles.title}>"Hi, Frank!" 👋</Text>
             </View>
-            <View style={styles.progress_box}>
-              <Text style={styles.progress_title}>Goal Weight</Text>
-              <Text style={styles.progress_value}>
-                {"160 lbs 🥅"}
-              </Text>
+            <View style={styles.progress_container}>
+              <View style={styles.progress_box}>
+                <Text style={styles.progress_title}>Current Weight:</Text>
+                <Text style={styles.progress_value}>
+                  {"155 lbs 🔼"}
+                </Text>
+              </View>
+              <View style={styles.progress_box}>
+                <Text style={styles.progress_title}>Goal Weight</Text>
+                <Text style={styles.progress_value}>
+                  {"160 lbs 🥅"}
+                </Text>
+              </View>
             </View>
           </View>
-        </View>
-        <View style={styles.exercise_container}>
-          <View
-            style={[
-              styles.title_box,
-              { backgroundColor: "rgba(178,108,233,1)", marginVertical: 10 },
-            ]}
-          >
-            <Text style={styles.title}>Workout Streak: 125 🔥</Text>
+          <View style={styles.exercise_container}>
+            <View
+              style={[
+                styles.title_box,
+                { backgroundColor: "rgba(178,108,233,1)", marginVertical: 10 },
+              ]}
+            >
+              <Text style={styles.title}>Workout Streak: 125 🔥</Text>
+            </View>
+            <ScrollView horizontal={false} style={styles.box}>
+              <Text>{friends}</Text>
+            </ScrollView>
           </View>
-          <ScrollView horizontal={false} style={styles.box}>
-            <Text>{friends}</Text>
-          </ScrollView>
-        </View>
-        <View style={styles.btn_box}>
-          <TouchableOpacity
-            onPress={() => {
-              this.props.navigation.navigate("Exercise", {
+          <View style={styles.btn_box}>
+            <TouchableOpacity
+              onPress={() => {
+                this.props.navigation.navigate("Exercise", {
+                  username: this.props.navigation.state.params.username,
+                  token: this.props.navigation.state.params.token,
+                });
+              }}
+              style={[styles.btn_shape, { marginHorizontal: 10 }]}
+            >
+              <Text style={styles.btn_text}>Add Friends</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.refresh()}
+              style={[
+                styles.btn_shape,
+                { backgroundColor: "rgba(153,50,245,1)", marginHorizontal: 10 },
+              ]}
+            >
+              <Text style={styles.btn_text}>Refresh</Text>
+            </TouchableOpacity>
+          </View>
+          {/* <TouchableOpacity
+            onPress={() =>
+              this.props.navigation.navigate("Profile", {
                 username: this.props.navigation.state.params.username,
                 token: this.props.navigation.state.params.token,
-              });
-            }}
-            style={[styles.btn_shape, { marginHorizontal: 10 }]}
+              })
+            }
+            style={[styles.btn_shape, { backgroundColor: "rgba(74,144,226,1)" }]}
           >
-            <Text style={styles.btn_text}>Add Friends</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => this.refresh()}
-            style={[
-              styles.btn_shape,
-              { backgroundColor: "rgba(153,50,245,1)", marginHorizontal: 10 },
-            ]}
+            <Text style={styles.btn_text}>View Profile</Text>
+          </TouchableOpacity> */}
+          {/* <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("Login")}
+            style={[styles.btn_shape, { backgroundColor: "red" }]}
           >
-            <Text style={styles.btn_text}>Refresh</Text>
-          </TouchableOpacity>
+            <Text style={styles.btn_text}>Log Out</Text>
+          </TouchableOpacity> */}
+          <View style={styles.container}>
+              <StatusBar hidden/>
+              <View style={{flexDirection: 'row', justifyContent: 'space-evenly', flexWrap: 'wrap', alignItems: 'center', top: 10}}>
+                  {donutData.map((p, i) => {
+                  return <Donut key={i} percentage={p.percentage} color={p.color} delay={500 + 100 * i} max={p.max} calorie={p.calorie} dataLabel={p.dataLabel}/>
+                  })}
+              </View>
+          </View>
         </View>
-        {/* <TouchableOpacity
-          onPress={() =>
-            this.props.navigation.navigate("Profile", {
-              username: this.props.navigation.state.params.username,
-              token: this.props.navigation.state.params.token,
-            })
-          }
-          style={[styles.btn_shape, { backgroundColor: "rgba(74,144,226,1)" }]}
-        >
-          <Text style={styles.btn_text}>View Profile</Text>
-        </TouchableOpacity> */}
-        {/* <TouchableOpacity
-          onPress={() => this.props.navigation.navigate("Login")}
-          style={[styles.btn_shape, { backgroundColor: "red" }]}
-        >
-          <Text style={styles.btn_text}>Log Out</Text>
-        </TouchableOpacity> */}
-        <View style={styles.container}>
-            <StatusBar hidden/>
-            <View style={{flexDirection: 'row', justifyContent: 'space-evenly', flexWrap: 'wrap', alignItems: 'center', top: 10}}>
-                {donutData.map((p, i) => {
-                return <Donut key={i} percentage={p.percentage} color={p.color} delay={500 + 100 * i} max={p.max} calorie={p.calorie} dataLabel={p.dataLabel}/>
-                })}
-            </View>
-        </View>
-      </View>
-    );
-  }
-}
+      );
+    }
+  
+
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: "column",
+      justifyContent: "flex-start",
+      alignItems: "center",
+    },
+    progress: {
+      width: "95%",
+      height: 125,
+      marginTop: 15,
+      alignItems: "center",
+    },
+    title_box: {
+      backgroundColor: "rgba(74,144,226,1)",
+      borderRadius: 10,
+      width: "95%",
+      height: 40,
+      justifyContent: "center",
+    },
+    title: {
+      color: "rgba(255,255,255,1)",
+      fontSize: 22,
+      alignSelf: "center",
+    },
+    progress_container: {
+      flexDirection: "row",
+      width: "100%",
+      marginTop: 20,
+      justifyContent: "center",
+    },
+    progress_box: {
+      backgroundColor: "rgba(213,218,223,1)",
+      width: "40%",
+      height: 55,
+      borderRadius: 10,
+      marginHorizontal: 25,
+    },
+    progress_title: {
+      color: "#121212",
+      alignSelf: "center",
+      marginVertical: 4,
+    },
+    progress_value: {
+      color: "#121212",
+      fontSize: 20,
+      fontWeight: "bold",
+      alignSelf: "center",
+    },
+    exercise_container: {
+      width: "95%",
+      height: 400,
+      alignItems: "center",
+    },
+    box: {
+      backgroundColor: "rgba(213,218,223,1)",
+      borderRadius: 10,
+      width: "95%",
+      height: 275,
+      alignSelf: "center",
+    },
+    btn_box: {
+      flexDirection: "row",
+      width: "75%",
+      justifyContent: "center",
+    },
+    btn_shape: {
+      backgroundColor: "rgba(178,108,233,1)",
+      borderRadius: 10,
+      width: "50%",
+      height: 40,
+      marginTop: 10,
+      justifyContent: "center",
+    },
+    btn_text: {
+      color: "rgba(255,255,255,1)",
+      fontSize: 16,
+      textAlign: "center",
+      fontWeight: "bold",
+    },
+  })
+
+    
 
 export default Home;
 // import React, {useState, useEffect} from 'react';
